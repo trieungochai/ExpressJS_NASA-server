9024ee726e669b4f4efce1c857cc0160cd94efb7
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-06-08 22:49:51 +0700

    CORS middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
 
+var whitelist = ["http://localhost:3000"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(planetsRouter);
 
