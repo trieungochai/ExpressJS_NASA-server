13674c9f8ec9a87dd86bcdbd45e5b0f7459cb091
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-06-17 17:33:27 +0700

    Working with Data Models: Building a Data Access Layer

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -1,9 +1,9 @@
-const { launches } = require("../../models/launches.model");
+const { getAllLaunches } = require("../../models/launches.model");
 
-function getAllLaunches(req, res) {
-  return res.status(200).json(Array.from(launches.values()));
+function httpGetAllLaunches(req, res) {
+  return res.status(200).json(gelAllLaunches());
 }
 
 module.exports = {
-  getAllLaunches,
+  httpGetAllLaunches,
 };
